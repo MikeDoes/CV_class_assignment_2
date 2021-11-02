a340a6c72f5e0283d5e3be14c25bf308dc2273e4
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2021-11-02 17:13:09 +0100

    Add files via upload

--- mean-shift_cow/mean-shift.py
+++ mean-shift_cow/mean-shift.py
@@ -14,23 +14,42 @@ def distance(x, X):
     #Make sure x is a tensor, not just an array:
     #tensor([[ 35.8645, -14.4873,  19.7296]], dtype=torch.float64)
 
-    return torch.cdist(x, X)
+    return torch.cdist(torch.unsqueeze(x, 0), X)
     
 
 def distance_batch(x, X):
-    return torch.cdist(x, X)
+    return torch.cdist(torch.unsqueeze(x, 0), X)
 
 def gaussian(dist, bandwidth):
+    
     #Bandwith parameter is squared, then doubled and divides the distance, all wrapped in an exponent function.
     
-    return torch.exp(-(2/bandwidth)**2 * dist)
+    #Each other point has a weight now
+    return torch.squeeze(torch.exp(-(2/bandwidth)**2 * dist))
 
 def update_point(weight, X):
-
-    return torch.sum(weight*X) / torch.sum(weight)
+    
+    weighted_sum = torch.matmul(weight,X)
+    weight_sum = torch.sum(weight)
+    normalised_weight = weighted_sum / weight_sum
+    
+    return normalised_weight
 
 def update_point_batch(weight, X):
-    return torch.sum(weight*X) / torch.sum(weight)
+    weighted_sum = torch.transpose(torch.matmul(weight,X), 0, 1)
+    
+    #print('weighted_sum should be 3675 x 3', weighted_sum.shape)
+
+    weight_sum = torch.sum(weight, dim = 1)
+
+    #print('weight_sum, should be 3675 x 1', weight_sum.shape)
+
+    #print('normalised_weight should be 3675 x 3')
+    normalised_weight = torch.div(weighted_sum, weight_sum)
+    
+    normalised_weight = torch.transpose(normalised_weight, 0, 1)
+
+    return normalised_weight
 
 def meanshift_step(X, bandwidth=2.5):
     X_ = X.clone()
@@ -41,14 +60,26 @@ def meanshift_step(X, bandwidth=2.5):
     return X_
 
 def meanshift_step_batch(X, bandwidth=2.5):
-    pass
+    X_ = X.clone()
+    
+    dist = distance_batch(X, X)
+    #Distance matrix is ok, 0 diagonal
+
+    weight = gaussian(dist, bandwidth)
+
+    X_ = update_point_batch(weight, X)
+    return X_
 
 def meanshift(X):
     X = X.clone()
     for _ in range(20):
-        X = meanshift_step(X)   # slow implementation
-        # X = meanshift_step_batch(X)   # fast implementation
+        #X = meanshift_step(X)   # slow implementation
+        X = meanshift_step_batch(X)   # fast implementation
+        print(f'step {_}')
     return X
+
+
+
 
 scale = 0.25    # downscale the image to run faster
 
@@ -60,10 +91,9 @@ image_lab = image_lab.reshape([-1, 3])  # flatten the image
 
 # Run your mean-shift algorithm
 t = time.time()
-X = meanshift(torch.from_numpy(image_lab)).detach().cpu().numpy()
+X = meanshift(torch.from_numpy(image_lab).cuda()).detach().cpu().numpy()
 
-print(X)
-# X = meanshift(torch.from_numpy(data).cuda()).detach().cpu().numpy()  # you can use GPU if you have one
+#X = meanshift(torch.from_numpy(data).cuda()).detach().cpu().numpy()  # you can use GPU if you have one
 t = time.time() - t
 print ('Elapsed time for mean-shift: {}'.format(t))
 
@@ -78,19 +108,3 @@ result_image = colors[labels].reshape(shape)
 result_image = rescale(result_image, 1 / scale, order=0, multichannel=True)     # resize result image to original resolution
 result_image = (result_image * 255).astype(np.uint8)
 io.imsave('result.png', result_image)
-
-
-#Pseudo Code
-""" 
-while not_converged():
-    for i, point in enumerate(points):
-        #distance for the given point to all points
-        distances = distance(point, points)
-
-        #turn distance into wights using a gaussian
-        weights = gaussian(dist, bandwidth = 2.5)
-
-        # update the points by calculating weightd mean of all points
-        points[i] = update_point(weight, X)
-
-return points """
